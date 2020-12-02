7f13f70bb55799e24d99f29a1770fd5e7a86557d
unknown <pierrick@live.co.uk>
2020-12-02 13:40:07 +0000

    Building the app

---
+++ calculator.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    int num1, num2;
+
+    cout << "Enter first num: ";
+    cin >> num1;
+
+    cout << "Enter second num: ";
+    cin >> num2;
+
+    cout << "Answer: " << num1 + num2 << endl;
+}
+
+// int addIntegers(int num1, int num2) {
+
+// }
+
